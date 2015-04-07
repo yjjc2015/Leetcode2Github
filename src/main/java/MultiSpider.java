84307d0a0b0169109bff863d15493a521e3f62cb
haodong <jikyll@163.com>
2015-04-07 13:42:56 -0400

    test for one thread

--- src/main/java/MultiSpider.java
+++ src/main/java/MultiSpider.java
@@ -4,6 +4,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import scheduler.DispatchScheduler;
 import downloader.HttpClientDownloader;
 
@@ -13,22 +16,29 @@ public class MultiSpider {
 	private ExecutorService service;
 	private String username;
 	private String password;
-	private DispatchScheduler scheduler;
+	private static DispatchScheduler scheduler;
+	private static Logger myLog;
 	
 	public MultiSpider create(String username, String password) {
+		PropertyConfigurator.configure("test.log");
+		myLog = Logger.getLogger(MultiSpider.class);
+		myLog.debug("开始创建MultiSpider");
 		this.username = username;
 		this.password = password;
 		HttpClientDownloader.init(username, password);
 		scheduler = new DispatchScheduler();
 		scheduler.startProcess();
+		myLog.debug("MultiSpider创建完毕");
 		return new MultiSpider();
 	}
 
 	public void run() {
+		myLog.debug("正式开始爬虫");
 		service = Executors.newFixedThreadPool(this.threadNum);
 		Future future = service.submit(new Callable() {
 
 			public Object call() throws Exception {
+				myLog.debug("进入Executors模块，准备进入taskProcceed方法");
 				scheduler.taskProcceed();
 				return null;
 			}
@@ -39,5 +49,10 @@ public class MultiSpider {
 	public MultiSpider thread(int num) {
 		this.threadNum = num;
 		return this;
+	}
+	
+	public static void main(String[] args) {
+		MultiSpider spider = new MultiSpider();
+		spider.create("tanghaodong25@163.com", "thd04180015").run();;
 	}
 }
