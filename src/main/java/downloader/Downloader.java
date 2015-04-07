806c128da40eeef133da7674bb131b5f4db74244
haodong <jikyll@163.com>
2015-04-07 13:43:59 -0400

    review downloader

--- src/main/java/downloader/Downloader.java
+++ src/main/java/downloader/Downloader.java
@@ -10,34 +10,27 @@ public interface Downloader {
 	public void setTread(Thread thread);
 	
 	/**
-	 * according to the url given, download aimed file(url or code) and add to list 
-	 * @param url
-	 * @param list
-	 */
-	public void download(String url);
-	
-	/**
 	 * 爬取题目列表
 	 * @param url
 	 * @return 
 	 */
-	public List<String> problemListDownloader();
+	public List<String> problemListDownloader(String pattern);
 	
 	/**
 	 * 进入题目描述页面，爬取题目提交url列表
 	 * @param url
 	 */
-	public List<String> problemDescriptionDownloader(String url);
+	public List<String> problemDescriptionDownloader(String url, String pattern);
 	
 	/**
 	 *	进入题目提交列表页面，爬取code所在页面url 
 	 * @param url
 	 */
-	public List<String> submissionListDownloader(String url, String name);
+	public List<String> submissionListDownloader(String url, String pattern);
 	
 	/**
 	 * 进入code所在页面，爬取code
 	 * @param url
 	 */
-	public List<String> codePageDownloader(String url);
+	public List<String> codePageDownloader(String url, String pattern);
 }
