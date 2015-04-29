161494c6660bb74f9354fd167c168a197ca1ffa0
haodong <tanghaodong25@gmail.com>
2015-04-29 07:46:34 -0400

    新增了Result类保存AC题目

---
+++ src/main/java/container/Result.java
@@ -0,0 +1,18 @@
+package container;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Result {
+	private static final List<String> list = new ArrayList<String>();
+	public static List<String> getResult() {
+		return list;
+	}
+	public String toString() {
+		StringBuilder string = new StringBuilder();
+		for (int i = 0; i < list.size(); i++) {
+			string.append(" ").append(list.get(i));
+		}
+		return string.toString();
+	}
+}
